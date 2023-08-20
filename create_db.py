df5378148d139aa6dccd788698a041c89ac6799a
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-08-20 22:00:20 +0100

    trans

---
+++ create_db.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
+
+# Add more classes for other tables in your database
+
