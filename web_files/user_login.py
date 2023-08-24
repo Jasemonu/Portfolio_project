f215bd8bdb7f6382d571b2ca57299caada1fd795
Rosemaryefebe247 <efebeoghenekaro@gmail.com>
2023-08-25 00:24:13 +0100

    file

--- web_files/user_login.py
+++ web_files/user_login.py
@@ -2,7 +2,7 @@
 """ objects that handle all default RestFul API actions for user_login """
 from models.user import User
 from models import storage
-from flask import abort, Flask, jsonify, render_template, redirect, url_for, request
+from flask import abort,current_app, Flask, jsonify, render_template, redirect, url_for, request
 from flask_login import current_user, login_user, login_required, logout_user, LoginManager
 from werkzeug.security import check_password_hash
 
@@ -11,6 +11,7 @@ app = Flask(__name__)
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
+
 
 @app.route('/home', methods=['GET'], strict_slashes=False)
 def home():
@@ -27,6 +28,13 @@ def tear_down(exception):
 
 
 @login_manager.user_loader
+def load_user(user_id):
+    # Load and return the user object based on the user_id
+    # This function is required by Flask-Login to retrieve users from the ID
+    # It should return the user object or None if the user doesn't exist
+    return User.query.get(user_id)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     """
@@ -40,24 +48,19 @@ def login():
     """
     The session searches for the email address of the user
     to check if its in the database"""
-    user = session.query(User).filter_by(email=email).first()
+    user = storage.get(User, id="email")
 
     """
     The user password is checked against the hashed password table
     which was created during registration
     """
-    if user and check_password_hash(user.password, password):
+    if user is not None and user.password:
         login_user(user)
         storage.close()
-        return redirect(url_for('home'))
+        return redirect(url_for('profile'))
     else:
         storage.close()
-        abort(400)
-
-    """
-    The GET request that renders the form tag in the login html template
-    for users to fill"""
-    return render_template('login.html')
+        return ("Invalid email or Password")
 
 
 @app.route('/profile', methods=['GET'])
