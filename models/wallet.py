61284d34dfcd90e4414ff480f03a1813654e5374
SamZec <samzec4@gmail.com>
2023-09-04 23:35:27 +0000

    Transfer endpoint done together with about

--- models/wallet.py
+++ models/wallet.py
@@ -9,11 +9,11 @@ class Wallet(BaseModel, Base):
     """creates wallet objects"""
     __tablename__ = "wallets"
     user_id = Column(String(120), ForeignKey("users.id"), nullable=True)
-    phone_number = Column(Integer, nullable=False)
+    phone_number = Column(String(15), nullable=False)
     pin = Column(Integer, nullable=False)
     next_of_kin = Column(String(120))
     next_of_kin_relationship = Column(String(120))
-    next_of_kin_number = Column(Integer)
+    next_of_kin_number = Column(String(15))
     balance = Column(Float, default=0.00)
     user = relationship("User", back_populates="wallet")
     transactions = relationship('Transaction', back_populates='wallet',
